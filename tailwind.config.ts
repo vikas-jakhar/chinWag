133c4a90aa0a49f9fd0aa2a8d00a475cd883d5df
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-09-02 14:12:08 +0530

    header update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        
+
       },
+      container: {
+        center: true,
+        padding: "20px",
+        screens: {
+          'sm': "640px",
+          'md': "768px",
+          'lg': "1024px",
+          'xl': "1180px",
+        }
+      }
     },
   },
   plugins: [],
