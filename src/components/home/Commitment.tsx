33c531f2620fb65801f2597fbcb5da8878ed2f74
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-09-05 15:22:47 +0530

    private section update

--- src/components/home/Commitment.tsx
+++ src/components/home/Commitment.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import PrimaryHeading from '../custom-ui/PrimaryHeading'
-import PrimaryParagraph from '../custom-ui/PrimaryParagraph'
-import Image from 'next/image'
-const Commitment = () => {
+import React from 'react';
+import PrimaryHeading from '../custom-ui/PrimaryHeading';
+import PrimaryParagraph from '../custom-ui/PrimaryParagraph';
+import Image from 'next/image';
+
+const Commitment: React.FC = () => {
     return (
         <div className='relative'>
             <div className='container pt-[145px] max-lg:pt-[137px] pb-[143px] max-sm:pt-[131px] max-md:pb-[110px] max-lg:pb-[120px] max-sm:pb-[94px]'>
@@ -10,7 +11,7 @@ const Commitment = () => {
                     <div className='relative max-lg:hidden'>
                         <Image
                             src="/assets/images/webp/commitment-image.webp"
-                            alt="On-Demand"
+                            alt="Commitment Image"
                             className="z-30 relative pointer-events-none"
                             width={489}
                             height={527}
@@ -18,7 +19,7 @@ const Commitment = () => {
                         />
                         <Image
                             src="/assets/images/webp/commitment-ellips.webp"
-                            alt="On-Demand"
+                            alt="Commitment Ellipse"
                             className="absolute top-[-40px] left-0 z-0 pointer-events-none"
                             width={489}
                             height={527}
@@ -27,14 +28,14 @@ const Commitment = () => {
                     </div>
                     <div className='flex flex-col max-lg:text-center'>
                         <PrimaryHeading text='Commitment to Transparency' className='text-offGreen max-w-[448px]' />
-                        <PrimaryParagraph data='Marketing platforms, and agencies, can inadvertently inflate your partnership costs by charging hidden fees. ' className='max-w-[404px] font-light pt-5 max-sm:pt-2' />
+                        <PrimaryParagraph data='Marketing platforms, and agencies, can inadvertently inflate your partnership costs by charging hidden fees.' className='max-w-[404px] font-light pt-5 max-sm:pt-2' />
                         <PrimaryParagraph data='We’ve put a stop to that problem.' className='pt-3 font-light' />
                         <PrimaryParagraph data='With charge-free & efficient matchmaking, we ensure partnership costs are never more than they would have otherwise been, even if created directly or through referral.' className='max-w-[426px] pt-3 max-sm:pt-2 font-light' />
                     </div>
                     <div className='relative lg:hidden pt-10'>
                         <Image
                             src="/assets/images/webp/commitment-image.webp"
-                            alt="On-Demand"
+                            alt="Commitment Image"
                             className="z-30 relative"
                             width={489}
                             height={527}
@@ -42,7 +43,7 @@ const Commitment = () => {
                         />
                         <Image
                             src="/assets/images/webp/commitment-ellips.webp"
-                            alt="On-Demand"
+                            alt="Commitment Ellipse"
                             className="absolute top-[-40px] z-0 left-0 pointer-events-none"
                             width={489}
                             height={527}
@@ -53,21 +54,22 @@ const Commitment = () => {
             </div>
             <Image
                 src="/assets/images/webp/plus-ellips.webp"
-                alt="On-Demand"
+                alt="Plus Ellipse"
                 className="absolute max-sm:hidden top-[380px] right-[30px]"
                 width={66}
                 height={78}
                 priority
             />
-            <Image src="/assets/images/webp/two-circle.webp"
-                alt="On-Demand"
+            <Image
+                src="/assets/images/webp/two-circle.webp"
+                alt="Two Circle"
                 className="absolute top-[131px] pointer-events-none left-[10px] max-sm:w-[26px] max-sm:h-[26px]"
                 width={66}
                 height={78}
                 priority
             />
         </div>
-    )
-}
+    );
+};
 
-export default Commitment
+export default Commitment;
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,41 +1,46 @@
-import React from 'react'
-import PrimaryParagraph from '../custom-ui/PrimaryParagraph'
-import Image from 'next/image'
+import React from 'react';
+import PrimaryParagraph from '../custom-ui/PrimaryParagraph';
+import Image from 'next/image';
 import Icon from '../common/Icons';
 import Header from '../common/Header';
+import Link from 'next/link';
 
-const Hero = () => {
+const Hero: React.FC = () => {
   return (
     <div className='relative'>
       <Header />
       <div className='container relative'>
-        <div className='flex justify-between flex-wrap max-xl:justify-center max-sm:items-center max-xl:text-center pt-[213px] max-sm:pt-[119px] pb-[141px] max-md:pb-[50px] max-sm:pb-[71px] max-xl:pb-[60px] max-sm:pb-0 items-center'>
-          <div className='relative z-30 transform translate-x-[-16%] translate_x max-xl:translate-x-0'>
+        <div className='flex justify-between flex-wrap max-xl:justify-center max-sm:items-center max-xl:text-center pt-[213px] max-sm:pt-[119px] pb-[141px] max-md:pb-[50px] max-sm:pb-[71px] max-xl:pb-[60px]'>
+          <div className='relative z-30 transform -translate-x-[16%] max-xl:translate-x-0'>
             <Image
               src="/assets/images/gif/hero-img.gif"
-              alt="On-Demand"
-              className="z-30 w-full relative rounded-lg shadow-gif-shadow max-w-[715px] max-sm:!h-[197px] !h-[456px]"
+              alt="Hero Image"
+              className="z-30 w-full rounded-lg shadow-gif-shadow max-w-[715px] max-sm:!h-[197px] !h-[456px]"
               width={715}
               height={456}
               priority
             />
             <Image
               src="/assets/images/webp/dot-img.webp"
-              alt="On-Demand"
+              alt="Dot Decoration"
               className="absolute z-0 top-[-49px] right-[-20px] max-sm:top-[-20px] max-sm:right-[-14px] max-sm:w-[52px] max-sm:h-auto"
               width={146}
               height={118}
-              sizes="(max-width: 640px) 52px, 146px" 
+              sizes="(max-width: 640px) 52px, 146px"
               priority
             />
           </div>
           <div className='flex flex-col max-xl:pt-16 max-sm:pt-[23px] max-xl:text-center max-xl:items-center'>
-            <h1 className=' font-black max-sm:text-[30px] relative z-30 text-offGreen max-sm:leading-[40px] max-w-[419px] max-sm:max-w-[273px] text-[46px] leading-[61px]'>Introductions that save you money.</h1>
-            <PrimaryParagraph data='Avoid inflated costs, keep your identity hidden while searching for Partners, and get rewarded for paying on-time.' className='max-w-[409px] pt-3 z-20' />
-            <button className='font-bold border-[1px] border-transparent hover:border-offGreen duration-500 hover:bg-white hover:text-offGreen text-base max-sm:mt-[21px] leading-5 max-w-[237px] text-nowrap text-white rounded-[65px] bg-darkGreen py-[16px] px-[20px] mt-[41px]'>Publish your Requirement</button>
+            <h1 className='font-black max-sm:text-[30px] relative z-30 text-offGreen max-sm:leading-[40px] max-w-[419px] max-sm:max-w-[273px] text-[46px] leading-[61px]'>
+              Introductions that save you money.
+            </h1>
+            <PrimaryParagraph data='Avoid inflated costs, keep your identity hidden while searching for partners, and get rewarded for paying on time.' className='max-w-[409px] pt-3 z-20' />
+            <button className='font-bold border-[1px] border-transparent hover:border-offGreen duration-500 hover:bg-white hover:text-offGreen text-base max-sm:mt-[21px] leading-5 max-w-[237px] text-nowrap text-white rounded-[65px] bg-darkGreen py-[16px] px-[20px] mt-[41px]'>
+              Publish your Requirement
+            </button>
           </div>
         </div>
-        <div className='max-2xl:hidden md:hidden'>
+        <div className='absolute hidden max-2xl:block md:hidden'>
           <div className='flex flex-col gap-[17px] max-sm:!gap-[4px] absolute right-[-112px] top-[107px]'>
             <Icon IconName='triangleIcon' />
             <Icon IconName='triangleIcon' />
@@ -45,7 +50,7 @@ const Hero = () => {
           </div>
           <Image
             src="/assets/images/webp/scroll-down.webp"
-            alt="On-Demand"
+            alt="Scroll Down"
             className="absolute right-[-103px] bottom-0"
             width={111}
             height={112}
@@ -53,26 +58,27 @@ const Hero = () => {
           />
         </div>
       </div>
-      <div className='absolute top-0 z-0 width-small w-full max-sm:h-[207px] max-lg:h-[407px] 2xl:w-[50%] xl:w-[683px] bg-darkGreen h-[400px] xl:h-[810px] xl:rounded-[0px_16px_16px_0px]'></div>
-      <div className='2xl:hidden'>
-        <div className='flex flex-col gap-[17px] max-sm:gap-[5px] max-md:left-[10px] max-md:top-[225px] absolute right-[15px] top-[107px]'>
+      <div className='absolute top-0 z-0 w-full max-sm:h-[207px] max-lg:h-[407px] 2xl:w-[50%] xl:w-[683px] bg-darkGreen h-[400px] xl:h-[810px] xl:rounded-[0px_16px_16px_0px]'></div>
+      <div className='flex'>
+        <div className='flex flex-col gap-[17px] max-sm:gap-[5px] absolute right-[15px] top-[107px]'>
           <Icon IconName='triangleIcon' className='max-sm:w-[4px] max-sm:h-[6px]' />
           <Icon IconName='triangleIcon' className='max-sm:w-[4px] max-sm:h-[6px]' />
           <Icon IconName='triangleIcon' className='max-sm:w-[4px] max-sm:h-[6px]' />
           <Icon IconName='triangleIcon' className='max-sm:w-[4px] max-sm:h-[6px]' />
           <Icon IconName='triangleIcon' className='max-sm:w-[4px] max-sm:h-[6px]' />
         </div>
-        <Image
-          src="/assets/images/webp/scroll-down.webp"
-          alt="sroll-down"
-          className="absolute max-sm:hidden right-[23px] bottom-0"
-          width={111}
-          height={112}
-          priority
-        />
+        <Link href="#benefit" className='w-fit absolute max-sm:hidden right-[23px] bottom-0'>
+          <Image
+            src="/assets/images/webp/scroll-down.webp"
+            alt="Scroll Down"
+            width={111}
+            height={112}
+            priority
+          />
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
