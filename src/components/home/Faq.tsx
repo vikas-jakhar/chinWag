9333ab0d55e60ad08869fa46a4c1fad1609beec4
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-09-10 14:23:03 +0530

    change

--- src/components/home/Faq.tsx
+++ src/components/home/Faq.tsx
@@ -13,10 +13,10 @@ const Faq: React.FC = () => {
         if (contentRefs.current[index]) {
             return contentRefs.current[index]?.scrollHeight + 'px';
         }
-        return '0px';
+        return '0px';        
     };
 
-    return (   
+    return (        
         <div className='z-[1] relative py-16 lg:py-24'>     
             <div className="container my-2.5 lg:mb-11 pb-0.5">
                 <PrimaryHeading text="FAQ’S" className="mx-auto text-center text-off-green" />
