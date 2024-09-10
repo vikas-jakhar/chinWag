7d2e9dea60e7ea657343ba9ab409468f8f735238
Your Name <nikhilbishnoibhadu@gmail.com>
2024-09-10 12:51:55 +0530

    page done

--- src/components/home/Private.tsx
+++ src/components/home/Private.tsx
@@ -7,8 +7,8 @@ const Private: React.FC = () => {
     return (
         <div className='relative'>
             <div className='container pt-[179px] max-sm:pt-[145px] max-md:pt-[100px] max-lg:pt-[160px]'>
-                <div className='flex justify-between flex-wrap w-full max-lg:justify-center lg:items-center'>
-                    <div className='flex flex-col max-lg:text-center'>
+                <div className='flex justify-between flex-wrap w-full max-lg:justify-center max-lg:items-center'>
+                    <div className='flex flex-col max-lg:text-center max-lg:items-center'>
                         <PrimaryHeading text='Stay Private. Stay in Control.' className='max-w-[397px] max-sm:max-w-[330px] text-off-green' />
                         <PrimaryParagraph data='Chin-wag empowers you with the ability to request information from the resource marketplace without revealing your identity.' className='max-w-[428px] font-light pt-5 max-sm:pt-[15px]' />
                         <PrimaryParagraph data='Review applications and schedule introductions with total discretion.' className='max-w-[428px] pt-4 max-sm:pt-2 font-light' />
@@ -42,7 +42,7 @@ const Private: React.FC = () => {
                 priority
             />
             <Image
-                src="/assets/images/webp/private-ellips.png"
+                src="/assets/images/webp/private-ellips.webp"
                 alt="Decorative Ellipse"
                 className="absolute pointer-events-none max-lg:bottom-0 max-w-[545px] lg:top-[130px] max-sm:bottom-[-40px] w-full lg:right-0 lg:left-auto md:left-0 z-0"
                 width={545}
